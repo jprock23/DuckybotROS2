0600733f568223ddaff2f79128153ac11df4a2c5
JP Roque <juanproque@outlook.com>
2025-06-03 13:55:01 -0500

    set up tf2 tree and robot_state_publisher

--- deliberation/setup.py
+++ deliberation/setup.py
@@ -1,4 +1,6 @@
-from setuptools import find_packages, setup
+import os
+from glob import glob
+from setuptools import setup, find_packages
 
 package_name = 'deliberation'
 
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.dae')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name), glob('urdf/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +26,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "broadcast=deliberation.static_broadcaster:main",
+            "chassis=deliberation.chassis_broadcaster:main",
+            "state_publisher=deliberation.robot_state_publisher:main"
         ],
     },
 )
---
+++ shared_utils/shared_utils/utils.py
@@ -0,0 +1,9 @@
+from math import sin, cos
+from geometry_msgs.msg import Quaternion
+
+def euler_to_quaternion(roll, pitch, yaw):
+    qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
+    qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2)
+    qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2)
+    qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)
+    return Quaternion(x=qx, y=qy, z=qz, w=qw)
