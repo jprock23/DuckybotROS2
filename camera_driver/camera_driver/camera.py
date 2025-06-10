5b6481e10ebe9378ef290f9d697462d9aa304f34
JP Roque <juanproque@outlook.com>
2025-06-10 12:33:10 -0500

    camera tweaks

--- camera_driver/camera_driver/camera.py
+++ camera_driver/camera_driver/camera.py
@@ -1,31 +1,36 @@
 import cv2
+import numpy
 
 class Camera:
     
     def __init__(self):
-        self.cam = cam = cv2.VideoCapture(0)
+        self.cam = cv2.VideoCapture(0, cv2.CAP_V4L2)
 
-        self.frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        
+        self.frame_width = int(self.cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.frame_height = int(self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        print("width:: ", self.frame_width)
+        print("height:: ", self.frame_height)
+
     def getFrame(self):
-        if cam.isOpened() == False:
-            print("Error")
+        if self.cam.isOpened() == False:
+            raise Exception()
 
-        ret, frame = cam.read()
+        ret, frame = self.cam.read()
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         if ret == True:
-            print(frame.shape)
-            cv2.imwrite('image.jpeg', frame)
-            
-        return frame
+          # return cv2.Mat(numpy.array(frame))
+          return frame
+
+        
+        #return cv2.Mat(numpy.zeros(shape=(self.frame_height, self.frame_width)))
     
-    def release(self):
-        cam.release()
+    def __del__(self):
+        self.cam.release()
         cv2.destroyAllWindows()
         
 
 # Open the default camera
-cam = cv2.VideoCapture(0)
+#cam = cv2.VideoCapture(0)
 
 # Get the default frame width and height
 #frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
--- camera_driver/camera_driver/camera_node.py
+++ camera_driver/camera_driver/camera_node.py
@@ -3,7 +3,9 @@ from .camera import Camera
 from rclpy.node import Node
 from cv_bridge import CvBridge
 
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, Image
+from std_msgs.msg import Header
+
 from interfaces.srv import GetImage
 
 class Camera_Node(Node):
@@ -13,12 +15,26 @@ class Camera_Node(Node):
         self.bridge = CvBridge()
         self.camera = Camera()
         
-        self.srv = self.create_service(GetImage, '/camera_img', self.cb)
+        #self.srv = self.create_service(GetImage, '/camera_img', self.cb)
+        self.publisher = self.create_publisher(Image, '/camera_img', 10)
+        self.Timer = self.create_timer(0.5, self.pcb)
+    
+    # def cb(self, request, response):
+    #     response.Image = self.bridge.cv2_to_compressed_imgmsg(self.camera.getFrame())
         
-    def cb(self, request, response):
-        response.Image = self.bridge.cv2_to_compressed_imgmsg(self.camera.getFrame())
+    #     return response
+    
+    def pcb(self):
+        msg = self.bridge.cv2_to_imgmsg(self.camera.getFrame(), 'mono8')
+        frame = self.camera.getFrame()
+        print(frame)
+        print(frame.shape)
+
+        msg.header = Header()
+        msg.header.stamp = self.get_clock().now().to_msg()
         
-        return response
+        self.publisher.publish(msg)
+        
         
 def main():
     rclpy.init()
@@ -26,8 +42,9 @@ def main():
     camera_node = Camera_Node()
     
     rclpy.spin(camera_node)
-    
+   
     rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
+
