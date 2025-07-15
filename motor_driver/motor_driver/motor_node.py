5aed891b6623e5535404a29f5b1328270a1d7df2
jproque <juanproque@outlook.com>
2025-07-15 13:44:25 -0500

    Added motor timeout

--- motor_driver/motor_driver/motor_node.py
+++ motor_driver/motor_driver/motor_node.py
@@ -17,14 +17,26 @@ class Motor_Node(Node):
         self.hat = Hat()
         self.left_motor = self.hat.get_motor(1, "left")
         self.right_motor = self.hat.get_motor(2, "right")
+
+        self.timer = self.create_timer(1.0, self.check_timeout)
+
+        self.last_msg_time = self.get_clock().now().to_msg()
         
-        
+    def check_timeout(self):
+        curr_time = self.get_clock().now().to_msg()
+
+        time_delta = (float(curr_time.sec) + (float(curr_time.nanosec/1e9))) - (float(self.last_msg_time.sec) + (float(self.last_msg_time.nanosec/1e9)))
+        if(time_delta > 1.0):
+            self.left_motor.set(0.0)
+            self.right_motor.set(0.0)
+
     def motor_cb(self, msg):
         """callback that subscirbes to the /wheels_cmd topic to get and apply motor controls"""
         self.get_logger().info(f'Time stamp: {msg.header.stamp.sec}, Frame_id: {msg.header.frame_id}, vel_left: {msg.vel_left}, vel_right: {msg.vel_right}')
             
         self.left_motor.set(msg.vel_left)
         self.right_motor.set(msg.vel_right)
+        self.last_msg = msg.header.stamp
         
     
 def main(args=None):
