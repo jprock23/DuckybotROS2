9330af548666b0ca59f2d08f7ec0dbef2c03dbef
jprock23 <juanproque@outlook.com>
2025-08-24 17:54:42 -0500

    added interfaces

--- motor_driver/motor_driver/motor_node.py
+++ motor_driver/motor_driver/motor_node.py
@@ -20,7 +20,7 @@ class Motor_Node(Node):
         self.cmd_subscription = self.create_subscription(WheelsCmdStamped, '/wheels_cmd', self.set_setpoint, 10)
         self.velocity_left_subscription = self.create_subscription(TwistStamped, '/left_encoder_node/velocity', self.update_left_vel, 10)
         self.velocity_right_subscription = self.create_subscription(TwistStamped, '/right_encoder_node/velocity', self.update_right_vel, 10)
-        self.throttle_subscription = self.create_subscription(Throttle, '/throttles', 10)
+        self.throttle_subscription = self.create_subscription(Throttle, '/throttles', self.set_throttles, 10)
 
         #Publishers
         self.executed_cmd_publisher= self.create_publisher(WheelsCmdStamped, '/wheels_cmd_executed', 10)
